b1de6a687ff5d0c4cfd6bd8fcf5d3587b284e7f6
mishfish123 <michelle.yu-ying.wong@monash.edu>
2020-09-01 09:31:13 +1000

    representative profile and other stuff

--- trial.py
+++ trial.py
@@ -4,4 +4,6 @@ import json
 response = requests.get('https://theyvoteforyou.org.au/api/v1/people/10001.json?key=k4Pwf%2BFaW2ygOTWEOCVn')
 json_data = json.loads(response.text)
 print(json_data['id'])
-print(json_data['latest_member']['name'])
+for policy in json_data['policy_comparisons']:
+    print(policy['policy']['name'])
+    print(policy['agreement'])
